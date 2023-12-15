cc7ee4e9fbb5c86c40b3199ee51c4f5688fc79d7
cpaul-mech <cpaul.mech@gmail.com>
2023-12-15 12:34:40 -0700

    Deep in the weeds abt timers in Arduino
    The previous code used a special kind of Timer Interrupt, that we can't use on an arduino. So instead we will be substituting in the TimerInterrupt class to regulate the run command

--- Code_for_Arduinos/MorphTestMotorControl_EDITED/MorphTestMotorControl_EDITED.ino
+++ Code_for_Arduinos/MorphTestMotorControl_EDITED/MorphTestMotorControl_EDITED.ino
@@ -1,10 +1,24 @@
+//Timer Library setup
+#include "TimerInterrupt.h"
+// The link to the repository is as follows: https://github.com/khoih-prog/TimerInterrupt?tab=readme-ov-file#important-notes-about-isr
+// NOTES ABOUT ISR:
+// Select the timers you're using, here ITimer1
+#define USE_TIMER_1     true
+#define USE_TIMER_2     false
+#define USE_TIMER_3     false
+#define USE_TIMER_4     false
+#define USE_TIMER_5     false
+TimerInterrupt ITimer1(1);
+// Init timer ITimer1
+
+void TimerHandler();
+
 
 //Radio Communication Setup
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-//Timer Library setup
-#include "TimerInterrupt.h"
+
 RF24 radio(15,14); // CE, CSN
 
 typedef enum {  RC_Zero, RC_SetDesiredPositionAll, RC_ResetRadio, RC_SetCurrentPosition, RC_ReadCurrentPosition, RC_SetDesiredPosition} RadioCommands_t;
@@ -13,23 +27,24 @@ const uint64_t pipes[25] = { 0xF0F0F0F0CDLL, 0xF0F0F0F061LL, 0xF0F0F0F081LL, 0xF
                               0xF0F0F0F029LL, 0xF0F0F0F069LL, 0xF0F0F0F089LL, 0xF0F0F0F0A9LL, 0xF0F0F0F0C9LL,
                               0xF0F0F0F02BLL, 0xF0F0F0F06BLL, 0xF0F0F0F08BLL, 0xF0F0F0F0ABLL, 0xF0F0F0F0CBLL,
                               0xF0F0F0F02DLL, 0xF0F0F0F06DLL, 0xF0F0F0F08DLL, 0xF0F0F0F0ADLL, 0xF0F0F0F0CFLL,};
-                              
+
 #define NodeID 0
 #define RF_Channel 2
 #define RadioResetRate_us 2000000
 
-//Motor Control Setup
+// Motor Control Setup
 #include "DCMotorControl.h"
 DCMotorControl Motors[] = {
-  DCMotorControl(16,17,5,2,3),    // Motor 0 DCMotorControl( uint8_t DirectionPin, uint8_t DrivePin, uint8_t Encoder1Pin, uint8_t Encoder2Pin)
+    DCMotorControl(16, 17, 5, 2, 3), // Motor 0 DCMotorControl( uint8_t DirectionPin, uint8_t DrivePin, uint8_t Encoder1Pin, uint8_t Encoder2Pin)
 
 };
-#define NumberOfMotors (sizeof(Motors)/sizeof(Motors[0]))
+#define NumberOfMotors (sizeof(Motors) / sizeof(Motors[0]))
 #define ControlRate_us 10000
+#define TIMER_INTERVAL_MS     10L // 10ms, or 10,000us as specfified by the ControlRate_us variable in the DCMotorControl.h file.
 #define DeadbandTicks 100
 #define DeadbandDutyCycle 0
-#define TicksPerInch ((50*64)/(3.14159265359*0.713))
-#define TicksPerRevolution (50*64)
+#define TicksPerInch ((50 * 64) / (3.14159265359 * 0.713))
+#define TicksPerRevolution (50 * 64)
 #define HomingSpeedTolerance 0.01
 #define MinimumPWM 0
 #define Kp 0.01
@@ -40,8 +55,8 @@ DCMotorControl Motors[] = {
 
 float DutyCycle = 0.0;
 float CurrentRPM = 0.0;
-int   MotorDrive[NumberOfMotors] = {0};
-bool  MotorEnabled = false;
+int MotorDrive[NumberOfMotors] = {0};
+bool MotorEnabled = false;
 float DesiredPosition[NumberOfMotors] = {0};
 int LastTicks = 0;
 int CurrentTicks = 0;
@@ -53,37 +68,42 @@ int counter = 0;
 // IntervalTimer ControlTimer;
 // IntervalTimer RadioResetTimer;
 void ControllerISR(void);
-//Debug Setup
-#define DEBUG 1
+// Debug Setup
 
-void setup() {
+void setup()
+{
   // radio.begin();
   // radio.openReadingPipe(0, pipes[NodeID]);
   // radio.setPALevel(RF24_PA_HIGH);
   // radio.setChannel(RF_Channel);
   // radio.startListening();
-
-  for(uint8_t i = 0; i < NumberOfMotors; i++){
+  if (ITimer1.attachInterruptInterval(TIMER_INTERVAL_MS, TimerHandler))
+    Serial.println("Starting  ITimer OK, millis() = " + String(millis()));
+  else
+    Serial.println("Can't set ITimer. Select another freq. or timer");
+  for (uint8_t i = 0; i < NumberOfMotors; i++)
+  {
     Motors[i].setParameters(Kp, Ki, Kd, ControlRate_us, DeadbandTicks, DeadbandDutyCycle, TicksPerInch, TicksPerRevolution, MinimumPWM);
     Motors[i].setDutyCycleStall(DutyCycleStall);
     Motors[i].setMaxDutyCycleDelta(MaxDutyCycleDelta);
-    
   }
 
-  for(uint8_t i = 0; i < (NumberOfMotors); i++){
+  for (uint8_t i = 0; i < (NumberOfMotors); i++)
+  {
     MotorEnabled = true;
     Motors[i].setMotorEnable(MotorEnabled);
     Motors[i].setMode(DC_Automatic);
   }
-  
-  #ifdef DEBUG
-    Serial.begin(9600);
-  #endif
-  
+
+#ifdef DEBUG
+  Serial.begin(9600);
+#endif
+
   // ControlTimer.begin( ControllerISR , ControlRate_us ); // attach the service routine here
   // RadioResetTimer.begin( RadioResetISR , RadioResetRate_us ); // attach the service routine here
 }
-void loop() {
+void loop()
+{
   // if (radio.available()) {
   //   RadioResponse();
   // }
@@ -98,7 +118,7 @@ void loop() {
   switch( inChar){
       case 'p':{
         radio.printDetails();
-        
+
         break;
       }
       case 'r':{
@@ -114,7 +134,7 @@ void loop() {
       }
       case 'f':{
           radio.flush_rx();
-          
+
           Serial.println("Flush RX");
           break;
       }
@@ -125,7 +145,7 @@ void loop() {
           break;
       }
       default:
-        break;        
+        break;
   }
     Serial.print(NodeID);
         Serial.print(" | Motor 0: ");
@@ -141,94 +161,100 @@ void loop() {
     //
     delay(100);
     */
-   Motors[0].setDesiredPositionTicks(1000);
-   Motors[0].run();
-     
+  Motors[0].setDesiredPositionTicks(1000);
+  
 }
 
-void RadioResponse(void){
-    long Ticks[8] = {0};
-    //RadioResetTimer.begin( RadioResetISR , RadioResetRate_us ); // attach the service routine here
-    Serial.println("Message Received");
-    radio.read(&Ticks, sizeof(Ticks));
-    Serial.print(Ticks[0]);
-    Serial.print(" ");
-    Serial.print(Ticks[1]);
-    Serial.print(" ");
-    Serial.println(Ticks[2]);
-    switch(Ticks[0]){
-      case RC_SetDesiredPositionAll:
-      {
-        for(uint8_t i = 0; i < (NumberOfMotors); i++){
-          Motors[i].setDesiredPositionTicks(Ticks[i+1]);
-        }      
-        
-      Serial.print(NodeID);
-      Serial.print(" | Motor 0: ");
-      Serial.print(Motors[0].getDesiredPositionTicks());
-      Serial.print("\t");
-      Serial.print(Motors[0].getCurrentPositionTicks());
-      Serial.print("\t");
-      Serial.print(Motors[0].getDutyCycle());
-      Serial.print("\t");
-      Serial.print(" | Motor 1: ");
-      Serial.print(Motors[1].getDesiredPositionTicks());
-      Serial.print("\t");
-      Serial.print(Motors[1].getCurrentPositionTicks());
-      Serial.print("\t");
-      Serial.print(Motors[1].getDutyCycle());
-      Serial.print("\t");
-      Serial.print("| radio debug | ");
-      Serial.print(radio.failureDetected);
-      Serial.print(" ");
-      Serial.print(radio.available());
-      Serial.println("");
-      
-        break;
-      }
-      case RC_Zero:
-      {
-        Serial.println("Zero");
-        if (0 == Ticks[1])
-        {
-          for(uint8_t i = 0; i < (NumberOfMotors); i++){
-            Motors[i].setCurrentPositionTicks(0);
-            Motors[i].setDesiredPositionTicks(0);
-          }
-        } else{
-          Motors[Ticks[1]-1].setCurrentPositionTicks(0);
-          Motors[Ticks[1]-1].setDesiredPositionTicks(0);
-        }
-        break;
-      }
-      case RC_ResetRadio:
-        {
-          radio.begin();
-          radio.failureDetected = 0;           // Reset the detection value
-          radio.openReadingPipe(0, pipes[NodeID]);
-          radio.setPALevel(RF24_PA_HIGH);
-          radio.startListening();
-          delay(10);
-          Serial.println("ResetRadio");
-          break;
-          
-        }
-      default:
-      break;
+void RadioResponse(void)
+{
+  long Ticks[8] = {0};
+  // RadioResetTimer.begin( RadioResetISR , RadioResetRate_us ); // attach the service routine here
+  Serial.println("Message Received");
+  radio.read(&Ticks, sizeof(Ticks));
+  Serial.print(Ticks[0]);
+  Serial.print(" ");
+  Serial.print(Ticks[1]);
+  Serial.print(" ");
+  Serial.println(Ticks[2]);
+  switch (Ticks[0])
+  {
+  case RC_SetDesiredPositionAll:
+  {
+    for (uint8_t i = 0; i < (NumberOfMotors); i++)
+    {
+      Motors[i].setDesiredPositionTicks(Ticks[i + 1]);
     }
+
+    Serial.print(NodeID);
+    Serial.print(" | Motor 0: ");
+    Serial.print(Motors[0].getDesiredPositionTicks());
+    Serial.print("\t");
+    Serial.print(Motors[0].getCurrentPositionTicks());
+    Serial.print("\t");
+    Serial.print(Motors[0].getDutyCycle());
+    Serial.print("\t");
+    Serial.print(" | Motor 1: ");
+    Serial.print(Motors[1].getDesiredPositionTicks());
+    Serial.print("\t");
+    Serial.print(Motors[1].getCurrentPositionTicks());
+    Serial.print("\t");
+    Serial.print(Motors[1].getDutyCycle());
+    Serial.print("\t");
+    Serial.print("| radio debug | ");
+    Serial.print(radio.failureDetected);
+    Serial.print(" ");
+    Serial.print(radio.available());
+    Serial.println("");
+
+    break;
   }
+  case RC_Zero:
+  {
+    Serial.println("Zero");
+    if (0 == Ticks[1])
+    {
+      for (uint8_t i = 0; i < (NumberOfMotors); i++)
+      {
+        Motors[i].setCurrentPositionTicks(0);
+        Motors[i].setDesiredPositionTicks(0);
+      }
+    }
+    else
+    {
+      Motors[Ticks[1] - 1].setCurrentPositionTicks(0);
+      Motors[Ticks[1] - 1].setDesiredPositionTicks(0);
+    }
+    break;
+  }
+  case RC_ResetRadio:
+  {
+    radio.begin();
+    radio.failureDetected = 0; // Reset the detection value
+    radio.openReadingPipe(0, pipes[NodeID]);
+    radio.setPALevel(RF24_PA_HIGH);
+    radio.startListening();
+    delay(10);
+    Serial.println("ResetRadio");
+    break;
+  }
+  default:
+    break;
+  }
+}
 
-
-void ControllerISR(void) {
+void ControllerISR(void)
+{
   // if we are tracking a trajectory, update the setpoint.
-  for(uint8_t i = 0; i < (NumberOfMotors); i++){
+  for (uint8_t i = 0; i < (NumberOfMotors); i++)
+  {
     Motors[i].run();
   }
 }
 
-
-void RadioResetISR(void) {
-  if (radio.available()) {
+void RadioResetISR(void)
+{
+  if (radio.available())
+  {
     RadioResponse();
   }
   radio.begin();
@@ -236,4 +262,9 @@ void RadioResetISR(void) {
   radio.setPALevel(RF24_PA_HIGH);
   radio.setChannel(RF_Channel);
   radio.startListening();
+}
+
+void TimerHandler(){
+  //I just want this function to run the motor.run function at the specfified period.
+  Motors[0].run();
 }
