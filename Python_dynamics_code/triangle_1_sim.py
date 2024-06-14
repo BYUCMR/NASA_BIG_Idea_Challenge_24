8fb00bc976b0789338e6748902cd294dd03caf6d
spencerstowell <stowel22@byu.edu>
2024-06-13 21:33:28 -0600

    Initial working triangle model

---
+++ Python_dynamics_code/triangle_1_sim.py
@@ -0,0 +1,68 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import nodeParam as P
+from triangle_dynamics_2D import TriangleDynamics
+from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation
+from RigidityMatrix2D import RigidityMatrix2D
+
+# import os
+# import imageio_ffmpeg as ffmpeg
+# os.environ['PATH'] += os.pathsep + ffmpeg.get_ffmpeg_exe()
+
+# TODO: Add subplot that shows the positions of the three nodes wrt time
+fig, ax = plt.subplots()
+line1, = ax.plot([], [], 'bo-', lw=2)   # Tube from node 1-2
+line2, = ax.plot([], [], 'bo-', lw=2)   # Tube from node 2-3
+line3, = ax.plot([], [], 'bo-', lw=2)   # Tube from node 3-1
+node1, = ax.plot([], [], 'ro', lw=2)    # Node 1
+node2, = ax.plot([], [], 'ro', lw=2)    # Node 2
+node3, = ax.plot([], [], 'ro', lw=2)    # Node 3
+ax.set_aspect('equal')
+ax.set_xlim(-0.5, 2)
+ax.set_ylim(-0.5, 2)
+ax.grid()
+
+# Initialize the triangle dynamics object
+triangle = TriangleDynamics()
+# Initialize the rigidity matrix object, used to initialize the node positions and edges
+RM = RigidityMatrix2D()
+
+# Initialization function
+def init():
+    line1.set_data([RM.x[0,0], RM.x[1,0]], [RM.x[0,1], RM.x[1,1]])
+    line2.set_data([RM.x[1,0], RM.x[2,0]], [RM.x[1,1], RM.x[2,1]])
+    line3.set_data([RM.x[2,0], RM.x[0,0]], [RM.x[2,1], RM.x[0,1]])
+    node1.set_data([RM.x[0,0], RM.x[0,1]])
+    node2.set_data([RM.x[1,0], RM.x[1,1]])
+    node3.set_data([RM.x[2,0], RM.x[2,1]])
+    return line1, line2, line3, node1, node2, node3
+
+# Update function for animation
+def update(frame):
+    tau = np.array([[0.0], [0.0], [100.0], [0.0], [0.0], [-50.0]])
+    triangle.update(tau)
+    x, y = triangle.h()
+    print(x, y)
+    line1.set_data([x[0], x[1]], [y[0], y[1]])
+    line2.set_data([x[1], x[2]], [y[1], y[2]])
+    line3.set_data([x[2], x[0]], [y[2], y[0]])
+    node1.set_data(x[0], y[0])
+    node2.set_data(x[1], y[1])
+    node3.set_data(x[2], y[2])
+    return line1, line2, line3, node1, node2, node3
+
+def save_animation(dynamic_animation):
+    f = r"C:/Users/stowel22/Desktop/animation.mp4"
+    FFMpegWriter = animation.writers['ffmpeg']
+    writer = FFMpegWriter(fps=30, metadata=dict(artist='Me'), bitrate=1800)
+    dynamic_animation.save(f, writer=writer)
+
+dynamic_animation = FuncAnimation(fig, update, frames=P.n_steps, init_func=init, blit=True, interval=1000*P.Ts)
+plt.title("Truss Animation")
+plt.xlabel("x")
+plt.ylabel("y")
+
+# Save the animation (may need to change the 'interval' parameter in dynamic_animcation to be 1 to get good frame rate)
+# save_animation(dynamic_animation)
+plt.show()
--- Python_dynamics_code/triangle_dynamics_2D.py
+++ Python_dynamics_code/triangle_dynamics_2D.py
@@ -1,12 +1,13 @@
 import numpy as np
-import trussParam2D as P
+import triangleParam2D as P
 from RigidityMatrix2D import RigidityMatrix2D
 
-class TrussDynamics:
+class TriangleDynamics:
     """
     This class outlines the dynamics of a single triangle truss structure
     """
-
+    # TODO: Add the ability to change the lengths of the tubes according to a kinematic solution
+    # Needs to be implemented within the 'f' function, likely through the RM object
     def __init__(self):
         # initial state conditions. Requires x, y, z, and xdot, ydot, and zdot for each of the 12 nodes
         # Order of the states: x, y, z for all nodes, followed by xdot, ydot, zdot for all nodes
@@ -19,7 +20,7 @@ class TrussDynamics:
         self.rk4_step(u)
         x = self.h()
     
-    def f(self, state, tau):
+    def f(self, state, tau=np.zeros((6, 1))):
         x1 = state[0][0]
         y1 = state[1][0]
         x2 = state[2][0]
@@ -55,9 +56,9 @@ class TrussDynamics:
         Fb[1] = -np.dot((np.array(x3, y3) - np.array([x2, y2])) /np.linalg.norm(np.array([x3, y3])-np.array([x2, y2])), np.array([x3_dot, y3_dot]) - np.array([x2_dot, y2_dot]))*P.b
         Fb[2] = -np.dot((np.array(x1, y1) - np.array([x3, y3])) /np.linalg.norm(np.array([x1, y1])-np.array([x3, y3])), np.array([x1_dot, y1_dot]) - np.array([x3_dot, y3_dot]))*P.b
 
-        # Construct the force vector
+        # Construct the force vector and add the external forces (6x1 column vector of forces applied in the x/y directions to nodes 1, 2, and/or 3)
         F = Fs + Fb
-        sum_of_forces = R_T @ F
+        sum_of_forces = R_T @ F + tau
         # sum of forces will create a column vector of the sum of forces for nodes 1, 2, and 3, in the x direction, followed by the y direction
 
         # Reconstruct the state vector using the equations of motion
@@ -68,9 +69,29 @@ class TrussDynamics:
         y2ddot = (1/P.m)*(sum_of_forces[4])
         y3ddot = (1/P.m)*(sum_of_forces[5])
 
+        # Impose a constraint on node 1 of zero x and y motion, and on node 2 of zero y motion
+        x1_dot = 0.0
+        y1_dot = 0.0
+        y2_dot = 0.0
+        x1ddot = 0.0
+        y1ddot = 0.0
+        y2ddot = 0.0
+
         # reconstruct the state vector
-        xdot = np.array([[x1_dot], [y1_dot], [x2_dot], [y2_dot], [x3_dot], [y3_dot], [x1ddot], [y1ddot], [x2ddot], [y2ddot], [x3ddot], [y3ddot]])
+        xdot = np.array([[x1_dot], [y1_dot], [x2_dot], [y2_dot], [x3_dot], [y3_dot], [x1ddot], [y1ddot], [x2ddot[0]], [y2ddot], [x3ddot[0]], [y3ddot[0]]])
         return xdot
+
+    def h(self):
+        '''
+        Function to collect and return the x and y positions of the three nodes
+
+        Returns:
+        x: np.array of x positions of the three nodes (1, 2, 3)
+        y: np.array of y positions of the three nodes (1, 2, 3)
+        '''
+        x = np.array([[self.state[0][0]], [self.state[2][0]], [self.state[4][0]]])
+        y = np.array([[self.state[1][0]], [self.state[3][0]], [self.state[5][0]]])
+        return x, y
 
     def rk4_step(self, tau):
         F1 = self.f(self.state, tau)
