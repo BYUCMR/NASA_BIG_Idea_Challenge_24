4b1604b7bcf10ff14b3fbda86f223210338db1b2
annieo29 <annie.obryan27@gmail.com>
2024-07-30 14:01:06 -0600

    Correct __init__ function, adjust time steps

--- Python_dynamics_code/Crane/crane_dynamics_kinematics.py
+++ Python_dynamics_code/Crane/crane_dynamics_kinematics.py
@@ -2,11 +2,16 @@ import numpy as np
 import crane_param as P
 from crane_rigidity_matrix import CraneRigidityMatrix
 
-with open('/Users/annieobryan/Desktop/NASA Big Idea Challenge/Integrated Kinematics and Dynamics/crane_positions.csv') as f:
+with open('/Users/annieobryan/Desktop/NASA Big Idea Challenge/Integrated Kinematics and Dynamics/crane.csv') as f:
     output = [float(s) for line in f.readlines() for s in line[:-1].split(',')]
 
-kinematic_positions = np.array(output).reshape(1002,90)
-kinematic_positions = kinematic_positions[:1000, :]
+kinematic_positions = np.array(output).reshape(305,594)
+x = kinematic_positions[:, :30]
+y = kinematic_positions[:, 198:228]
+z = kinematic_positions[:, 396:426]
+
+kinematic_positions = np.hstack([x, y, z])
+print(kinematic_positions.shape)
 
 class CraneDynamics:
     """
@@ -18,13 +23,13 @@ class CraneDynamics:
     def __init__(self):
         # Initial state condition. Requires x, y, z, and xdot, ydot, and zdot for each of the nodes
         # Order of the states: x, y, z for all nodes, followed by xdot, ydot, zdot for all nodes
+        self.RM = CraneRigidityMatrix()
         self.num_nodes = int(np.size(self.RM.x)/3)
         initial_node_positions = kinematic_positions[0, :]
         x_i = np.zeros((self.num_nodes, 3))
         for i in range(self.num_nodes):
             x_i[i] = np.array([initial_node_positions[i], initial_node_positions[i + self.num_nodes], initial_node_positions[i + 2*self.num_nodes]])
 
-        self.RM = CraneRigidityMatrix()
         x = x_i
         num_states = np.size(x)*2
         self.mag = self.RM.Get_Lengths()
@@ -40,6 +45,7 @@ class CraneDynamics:
         
         self.Ts = P.Ts
         self.i = 0
+        self.j = 0
 
     def update(self, u):
         self.rk4_step(u)
@@ -69,10 +75,10 @@ class CraneDynamics:
         # TODO: Update the rigidity matrix based on the kinematics files (matlab or python)
         # Get the magnitudes of each side length
 
-        if self.i < 1000:
-            x_kinematics = kinematic_positions[self.i :]
+        if self.i < 305:
+            x_kinematics = kinematic_positions[self.i, :]
         else:
-            x_kinematics = kinematic_positions[999, :]
+            x_kinematics = kinematic_positions[304, :]
         x_k = np.zeros((self.num_nodes, 3))
         for i in range(self.num_nodes):
             x_k[i] = np.array([x_kinematics[i], x_kinematics[i + self.num_nodes], x_kinematics[i + 2*self.num_nodes]])
@@ -135,7 +141,11 @@ class CraneDynamics:
         constraints = np.array([[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]])
 
         xdot = self.constrain(xdot, ground_nodes, constraints)
-        self.i += 1
+        if self.j == 1:
+            self.j = 0
+            self.i += 1
+        else:
+            self.j += 1
         
         return xdot
     
