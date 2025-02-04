325928feaba9aec16497ad425ff7079b4a1a4fe1
Christopher Paul <124709632+cpaul-mech@users.noreply.github.com>
2025-02-04 14:29:57 -0700

    Removed all children from Roller_00001

--- Revised_Arduino_Code/src/ROLLER_00001.cpp
+++ Revised_Arduino_Code/src/ROLLER_00001.cpp
@@ -5,11 +5,13 @@
 
 uint8_t roller_num = 1;
 Parent parent = {create_address(0,1), false, 0};
-Child child1 = {create_address(1,2), false, 1};
-Child child2 = {create_address(1,3), false, 2};
-Child children[] = {child1,child2};
+// Child child1 = {create_address(1,2), false, 1};
+// Child child2 = {create_address(1,3), false, 2};
+// Child children[] = {child1,child2};
+Child children[] = {};
 
-const uint8_t NUM_CHILDREN = sizeof(children)/sizeof(children[0]);
+// const uint8_t NUM_CHILDREN = sizeof(children)/sizeof(children[0]);
+const uint8_t NUM_CHILDREN = 0;
 
 Roller_Settings settings = {};
 
